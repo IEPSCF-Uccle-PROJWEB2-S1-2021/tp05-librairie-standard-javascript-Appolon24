c00ce7189eb951bfff718c086e3ad68499fea2ea
Appolon24 <71607931+Appolon24@users.noreply.github.com>
2020-10-12 18:45:20 +0200

    final

--- exercice1.js
+++ exercice1.js
@@ -1,3 +1,36 @@
 function chuckNorris(msg) {
+  if(msg.length > 0 && msg.length < 100){
+    let binaryCode = "";
+    let unaryCode = "";
+    for(i in msg){
+    binaryCode += msg[i].charCodeAt(0).toString([2]).padStart(7,"0");
+    }
+    let tableau = binaryCode.split("");
+    if (tableau[0] === '1'){
+      unaryCode += "0 0";
+    }
+    else{
+      unaryCode += "00 0"
+    }
+    for(let a = 1; a < tableau.length; a++){
+      if(tableau[a] === tableau[(a-1)]){
+        unaryCode += "0";
+      }
+      else{
+        if(tableau[a] === '1'){
+          unaryCode += " 0 0";
+        }
+        else{
+          unaryCode += " 00 0";
+        }
+      }
+    }
+    return unaryCode;
+  }
+  else{
   return "";
+      }
 }
+
+console.log(chuckNorris("C"));
+console.log(chuckNorris("CC"));
